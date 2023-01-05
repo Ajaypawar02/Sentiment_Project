3cbc15a794fffd509196162f261144fd1dc7b38a
Ajaypawar02 <ajaypawar1211@gmail.com>
2023-01-05 22:00:50 +0530

    Fourth Commit

--- model.py
+++ model.py
@@ -33,6 +33,24 @@ gc.enable()
 from args import args
 
 
+class WeightedLayerPooling(nn.Module):
+    def __init__(self, num_hidden_layers, layer_start = 8, layer_weights = None):
+        super(WeightedLayerPooling, self).__init__()
+        self.layer_start = layer_start
+        self.num_hidden_layers = num_hidden_layers
+        self.layer_weights = layer_weights if layer_weights is not None \
+            else nn.Parameter(
+                torch.tensor([1] * (num_hidden_layers+1 - layer_start), dtype=torch.float)
+            )
+
+    def forward(self, all_hidden_states):
+        all_layer_embedding = all_hidden_states[self.layer_start:, :, :, :]
+        weight_factor = self.layer_weights.unsqueeze(-1).unsqueeze(-1).unsqueeze(-1).expand(all_layer_embedding.size())
+        #print(weight_factor.shape)
+        weighted_average = (weight_factor*all_layer_embedding).sum(dim=0) / self.layer_weights.sum()
+        return weighted_average
+
+
 
 class SEN_Model(nn.Module):
     def __init__(self):
